20e3e9dcff6ce001ebc98ff1444c74d7cbf7fe33
VrundaPatel1400 <154300472+VrundaPatel1400@users.noreply.github.com>
2023-12-19 10:44:45 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,1 +1,10 @@
-
+n = int (input ("Enter the number:")
+fact = 1
+if n < 0:
+  print (“Factorial cannot be calculated, non-integer input”)
+elif n == 0:
+  print (“Factorial of the number is 1”)
+else:
+  for i in range (1, n+1):
+       fact = fact *i
+    print (“Factorial is: “, fact)
