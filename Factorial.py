a7e220179a6d91d0473b6fc14644236363f292f8
VrundaPatel1400 <154300472+VrundaPatel1400@users.noreply.github.com>
2023-12-19 10:47:24 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -3,8 +3,8 @@ fact = 1
 if n < 0:
   print ("Factorial does not exist for negative numbers")
 elif n == 0:
-  print (“Factorial of the number is 1”)
+  print ("Factorial of the number is 1")
 else:
   for i in range (1, n+1):
        fact = fact *i
-    print (“Factorial is: “, fact)
+    print ("Factorial is: ", fact)
